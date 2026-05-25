176b9094dc677c352fbd1d7d90f74d68eb07cc8f
deepika <deepika.kamble@techno.soft.in>
2026-05-25 13:08:19 +0530

    changes in api.js

--- api/api.js
+++ api/api.js
@@ -1,6 +1,6 @@
 const API_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5000"
-    : "https://maharashtrian-food.netlify.app";
+    : "https://maharashtrian-foodstore.onrender.com";
 
 export default API_URL;
