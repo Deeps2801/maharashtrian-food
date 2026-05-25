ab15d6bdda5e26fc27a92cd9f372c843580d0cee
deepika <deepika.kamble@techno.soft.in>
2026-05-25 14:48:51 +0530

    changes dbconnect.js

--- api/dbconnect.js
+++ api/dbconnect.js
@@ -1,18 +1,27 @@
 require("dotenv").config();
 const mysql = require("mysql2");
 
-const isLocal = process.env.NODE_ENV === "online";
-const db = mysql.createConnection({
-  host: isOnline ? process.env.ONLINE_DB_HOST : process.env.LOCAL_DB_HOST,
-  user: isOnline ? process.env.ONLINE_DB_USER : process.env.LOCAL_DB_USER,
-  password: isOnline ? process.env.ONLINE_DB_PASSWORD : process.env.LOCAL_DB_PASSWORD,
-  database: isOnline ? process.env.ONLINE_DB_NAME : process.env.LOCAL_DB_NAME,
-  port: isOnline ? process.env.ONLINE_DB_PORT : 3306,
-});
- 
+const isOnline = process.env.NODE_ENV === "online";
+
+let db;
+
+if (isOnline) {
+  // ONLINE (Railway)
+  db = mysql.createConnection(process.env.MYSQL_PUBLIC_URL);
+} else {
+  // LOCAL
+  db = mysql.createConnection({
+    host: process.env.LOCAL_DB_HOST,
+    user: process.env.LOCAL_DB_USER,
+    password: process.env.LOCAL_DB_PASSWORD,
+    database: process.env.LOCAL_DB_NAME,
+    port: 3306,
+  });
+}
+
 db.connect((err) => {
   if (err) {
-    console.log(err);
+    console.log("DB ERROR:", err);
   } else {
     console.log("Database Connected");
   }
